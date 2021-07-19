e24c78d516885ea67d7eae95f58d7b2ceb56fc71
Tyler Campbell <thehaqua@gmail.com>
2021-07-18 23:01:02 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JS-Quiz
+
+Quiz with temp questions for Javascript. Includes a timer but I was unable to work out the High-score function.
+
+https://thehaqua.github.io/JSQuiz/
---
+++ script.js
@@ -0,0 +1,274 @@
+var questions = [{
+    question: "1. Inside which HTML element do we put the JavaScript?",
+    choices: ["js", "script", "javascript", "scripting"],
+    correctAnswer: 1
+}, {
+    question: "2. Inside which HTML element do we put the JavaScript?",
+    choices: ["js", "script", "javascript", "scripting"],
+    correctAnswer: 1
+}, {
+    question: "3. Inside which HTML element do we put the JavaScript?",
+    choices: ["js", "script", "javascript", "scripting"],
+    correctAnswer: 1
+}, {
+    question: "4. Inside which HTML element do we put the JavaScript?",
+    choices: ["js", "script", "javascript", "scripting"],
+    correctAnswer: 1
+}, {
+    question: "5. Inside which HTML element do we put the JavaScript?",
+    choices: ["js", "script", "javascript", "scripting"],
+    correctAnswer: 1
+}, {
+    question: "6. Inside which HTML element do we put the JavaScript?",
+    choices: ["js", "script", "javascript", "scripting"],
+    correctAnswer: 1
+}, {
+    question: "7. Inside which HTML element do we put the JavaScript?",
+    choices: ["js", "script", "javascript", "scripting"],
+    correctAnswer: 1
+}, {
+    question: "8. Inside which HTML element do we put the JavaScript?",
+    choices: ["js", "script", "javascript", "scripting"],
+    correctAnswer: 1
+}, {
+    question: "9. Inside which HTML element do we put the JavaScript?",
+    choices: ["js", "script", "javascript", "scripting"],
+    correctAnswer: 1
+}, {
+    question: "10. Where is the correct place to insert a JavaScript?",
+    choices: ["The body section", "Both the head section and the body section are correct", "The head section", "The CSS file"],
+    correctAnswer: 1
+}];
+
+
+var currentQuestion = 0;
+var viewingAns = 0;
+var correctAnswers = 0;
+var quizOver = false;
+var iSelectedAnswer = [];
+	var c=180;
+	var t;
+$(document).ready(function () 
+{
+    
+    displayCurrentQuestion();
+    $(this).find(".quizMessage").hide();
+    $(this).find(".preButton").attr('disabled', 'disabled');
+	
+	timedCount();
+	
+	$(this).find(".preButton").on("click", function () 
+	{		
+		
+        if (!quizOver) 
+		{
+			if(currentQuestion == 0) { return false; }
+	
+			if(currentQuestion == 1) {
+			  $(".preButton").attr('disabled', 'disabled');
+			}
+			
+				currentQuestion--;
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					
+				} 					
+		} else {
+			if(viewingAns == 3) { return false; }
+			currentQuestion = 0; viewingAns = 3;
+			viewResults();		
+		}
+    });
+
+	
+	// On clicking next, display the next question
+    $(this).find(".nextButton").on("click", function () 
+	{
+        if (!quizOver) 
+		{
+			
+            var val = $("input[type='radio']:checked").val();
+
+            if (val == undefined) 
+			{
+                $(document).find(".quizMessage").text("Please select an answer");
+                $(document).find(".quizMessage").show();
+            } 
+			else 
+			{
+                
+                $(document).find(".quizMessage").hide();
+				if (val == questions[currentQuestion].correctAnswer) 
+				{
+					correctAnswers++;
+				}
+				iSelectedAnswer[currentQuestion] = val;
+				
+				currentQuestion++; 
+				if(currentQuestion >= 1) {
+					  $('.preButton').prop("disabled", false);
+				}
+
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					
+				} 
+                
+				else 
+				{
+					displayScore();
+					$('#iTimeShow').html('Quiz Time Completed!');
+					$('#timer').html("You scored: " + correctAnswers + " out of: " + questions.length);
+					c=185;
+					$(document).find(".preButton").text("View Answer");
+					$(document).find(".nextButton").text("Play Again?");
+					quizOver = true;
+					return false;
+					
+				}
+			}
+					
+		}	
+		else 
+		{ 
+			quizOver = false; $('#iTimeShow').html('Time Remaining:'); iSelectedAnswer = [];
+			$(document).find(".nextButton").text("Next Question");
+			$(document).find(".preButton").text("Previous Question");
+			 $(".preButton").attr('disabled', 'disabled');
+			resetQuiz();
+			viewingAns = 1;
+			displayCurrentQuestion();
+			hideScore();
+		}
+    });
+});
+
+
+
+function timedCount()
+	{
+		if(c == 185) 
+		{ 
+			return false; 
+		}
+		
+		var hours = parseInt( c / 3600 ) % 24;
+		var minutes = parseInt( c / 60 ) % 60;
+		var seconds = c % 60;
+		var result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);            
+		$('#timer').html(result);
+		
+		if(c == 0 )
+		{
+					displayScore();
+					$('#iTimeShow').html('Quiz Time Completed!');
+					$('#timer').html("You scored: " + correctAnswers + " out of: " + questions.length);
+					c=185;
+					$(document).find(".preButton").text("View Answer");
+					$(document).find(".nextButton").text("Play Again?");
+					quizOver = true;
+					return false;
+					
+		}
+		c = c - 1;
+		t = setTimeout(function()
+		{
+			timedCount()
+		},1000);
+	}
+	
+	
+
+function displayCurrentQuestion() 
+{
+
+	if(c == 185) { c = 180; timedCount(); }
+
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+
+    $(questionClass).text(question);
+
+    $(choiceList).find("li").remove();
+    var choice;
+	
+	
+    for (i = 0; i < numChoices; i++) 
+	{
+        choice = questions[currentQuestion].choices[i];
+		
+		if(iSelectedAnswer[currentQuestion] == i) {
+			$('<li><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+		} else {
+			$('<li><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+		}
+    }
+}
+
+function resetQuiz()
+{
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+
+function displayScore()
+{
+    $(document).find(".quizContainer > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+    $(document).find(".quizContainer > .result").show();
+}
+
+function hideScore() 
+{
+    $(document).find(".result").hide();
+}
+
+
+function viewResults() 
+{
+
+	if(currentQuestion == 10) { currentQuestion = 0;return false; }
+	if(viewingAns == 1) { return false; }
+
+	hideScore();
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+   
+    $(questionClass).text(question);
+
+    $(choiceList).find("li").remove();
+    var choice;
+	
+	
+	for (i = 0; i < numChoices; i++) 
+	{
+        choice = questions[currentQuestion].choices[i];
+		
+		if(iSelectedAnswer[currentQuestion] == i) {
+			if(questions[currentQuestion].correctAnswer == i) {
+				$('<li style="border:2px solid green;margin-top:10px;"><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			} else {
+				$('<li style="border:2px solid red;margin-top:10px;"><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			}
+		} else {
+			if(questions[currentQuestion].correctAnswer == i) {
+				$('<li style="border:2px solid green;margin-top:10px;"><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			} else {
+				$('<li><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			}
+		}
+    }
+	
+	currentQuestion++;
+	
+	setTimeout(function()
+		{
+			viewResults();
+		},3000);
+}
